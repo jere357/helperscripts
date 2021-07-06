5c9430a0052de1a24d1e5eba6b2a8a86ae9531d4
jere357 <jeronim96@gmail.com>
2021-07-06 12:56:02 +0200

    Add files via upload
    bolje formatiranje stringova u downscale i clip funkcijama

--- megaskripta.py
+++ megaskripta.py
@@ -4,8 +4,8 @@ import sys
 import os
 import time
 import math
-from multiprocessing.dummy import Pool as ThreadPool
 from itertools import repeat
+from progress.bar import IncrementalBar
 """
 function used for creating splicer subframes
 """
@@ -31,6 +31,19 @@ helper function for map(subscreen) call
 def subscreen(videowriter, pixel_range, image):
     videowriter.write(image[pixel_range[1][0] : pixel_range[1][1], pixel_range[0][0] : pixel_range[0][1]])
     return
+def calculate_times(time_skip, duration):
+    begin_hours = math.floor(time_skip/3600)
+    if begin_hours/10 < 1:
+        begin_hours = '0' + str(begin_hours)
+    begin_minutes = math.floor(time_skip/60)
+    if begin_minutes/10 < 1:
+        begin_minutes = '0' + str(begin_minutes)
+    begin_seconds = time_skip%60
+    if begin_seconds/10 <1:
+        begin_seconds = '0' + str(begin_seconds)
+    begin_time = str(begin_hours) + ':' + str(begin_minutes) + ':'+ str(begin_seconds)
+    #print(begin_time)
+    return begin_time
 """
 helper function s for extracting gray frames with pixel ranges from a single 4k frame, used in optical flow
 """
@@ -70,7 +83,9 @@ def stitch_video(filename, fourcc, frame_cap = (1000, 600), folder_name = 'stitc
     print("vrijeme za video 10s {} ".format(round(t2-t1,5)))
     videowriter.release()
     cap.release()
-
+"""
+idk jel ovo radi ne znan
+"""
 def slice_video_optical_flow_fast(filename, fourcc, frame_cap = (1000,600), folder_name = 'sliced_videos', time_skip = 0, duration = 7):
     video_folder_name = folder_name+ '/' + filename[:-4] + '_' + str(time_skip) + str(duration)
     try:
@@ -116,7 +131,6 @@ def slice_video_optical_flow_fast(filename, fourcc, frame_cap = (1000,600), fold
 
     
     pass
-
 """
 this function behaves in the same way as slice_video but only applies the optical flow calculation to every frame. SLOW version atm
 """
@@ -210,6 +224,50 @@ def slice_video_optical_flow(filename, fourcc, frame_cap = (1000,600), folder_na
         out.release()
         cv.destroyAllWindows()
 """
+
+"""
+def downscale_video(filename, folder_name = 'downscaled_videos', target_resolution = 1000):
+    video_folder_name = folder_name+ '/' + filename[:-4]
+    try:
+        os.mkdir(folder_name)
+    except OSError as error:
+        pass
+        #print(str(error) + " sliced videos folder already exists - irrelevant error")
+    try:
+        os.mkdir(video_folder_name)
+    except OSError as error:
+        pass
+    new_video_name = video_folder_name + '/' + filename[:-4] + '_lowres17.mp4'
+    #ffmpeg -i DJI_0963.MP4 -c:v libx264 -crf 24 clipped_videos/videotest.mp4 <- how it should look in the terminal
+    command = f'ffmpeg -i {filename} -filter:v scale={target_resolution}:-2 -c:v libx264 -crf 10 {new_video_name}'
+    print(command)
+    os.system(command)
+
+"""
+extract a clip from time_skip to duration seconds from filename and creates a subfolder in folder_name where it stores the video encoded with the fourcc
+if you set the target_resolution parameter to 1000 this function downscales the video to 1000x528 as well (528 not 600 is in order to preserve the aspect ratio)
+if target_resolution = 0 then it doesnt change the resolution while clipping the video
+"""
+def clip_video(filename, folder_name = 'clipped_videos', time_skip = 0, duration = 0, target_resolution = 0):
+    video_folder_name = folder_name+ '/' + filename[:-4]
+    try:
+        os.mkdir(folder_name)
+    except OSError as error:
+        pass
+    try:
+        os.mkdir(video_folder_name)
+    except OSError as error:
+        pass
+    new_video_name = video_folder_name + '/' + filename[:-4] + 'CLIP' + str(time_skip) + "_" + str(duration) + '.mp4'
+    #ffmpeg -ss 65 -i DJI_0963.MP4 -t 10 -c:v libx264 -crf 24 clipped_videos/videotest.mp4 <- how it should look in the terminal
+    #command =  "ffmpeg -ss " + str(time_skip)  + " -i " + filename + " -t " + str(duration) + " -c:v libx264 -crf 24 " + str(new_video_name)
+    if target_resolution == 0:
+        command = f'ffmpeg -ss {time_skip} -i {filename} -t {duration} -c:v libx264 -crf 10 {new_video_name}'
+    else:
+        command = f'ffmpeg -ss {time_skip} -i {filename} -t {duration} -filter:v scale={target_resolution}:-2 -c:v libx264 -crf 10 {new_video_name}'
+    print(command)
+    os.system(command)
+"""
 filename str - path to video you want to slice
 frame_cap (int,int) - maximum size of window 
 folder name str - name of the subfolder where the sliced videos are stored  folder_name/filenameXX.avi where X goes from 0 to X
@@ -227,10 +285,10 @@ def slice_video(filename, fourcc, frame_cap = (1000,600), folder_name = 'sliced_
         pass
         #print(str(error) + " video named subfolder already exists - irrelevant error")
     cap = cv.VideoCapture(filename)
-    fourcc = cv.VideoWriter_fourcc(*'mp4v')
     fps = int(cap.get(cv.CAP_PROP_FPS))
     video_size = (int(cap.get(3)), int(cap.get(4)))
     pixel_ranges = ranges(frame_cap, video_size)
+    print(pixel_ranges)
     cap.release()
     videowriters = []
     for counter, pixel_range in enumerate(pixel_ranges):
@@ -243,8 +301,10 @@ def slice_video(filename, fourcc, frame_cap = (1000,600), folder_name = 'sliced_
     cap.set(1, time_skip*fps)
     t1 = time.time()
     cnt=0
+    bar = IncrementalBar('Slicing video', max = fps*duration, suffix = '%(percent).1f%% - %(eta)ds')
     while cap.isOpened():
         cnt+=1
+        bar.next()
         if cnt == fps*duration and duration!=0:
             break 
         ret, frame = cap.read()
@@ -255,19 +315,20 @@ def slice_video(filename, fourcc, frame_cap = (1000,600), folder_name = 'sliced_
         #t4=time.time()
         #print("vrijeme za jedan frejm {}".format(round(t4-t3,5)))
     t2 = time.time()
-    print(str(round(t2-t1)) + "s for video")
+    print("\n" + str(round(t2-t1)) + "s for video")
     [vw.release() for vw in videowriters]
     cap.release()
-    cv.destroyAllWindows()
+    bar.finish()
+"""
+json parsiranje malo lgn drgy
+"""
+def json_parser():
+    
+    pass
+
 if __name__ == '__main__':
     folder_name = 'sliced_videos'
     filename = sys.argv[1]
-    frame_cap = (1000,600)
+    frame_cap = (3840,2160)
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
-    stitch_video(filename, fourcc, frame_cap)
-"""
-    slice_video(filename, fourcc, frame_cap, time_skip = 59, duration = 11)
-    slice_video(filename, fourcc, frame_cap, time_skip = 75, duration = 10)
-    slice_video(filename, fourcc, frame_cap, time_skip = 123, duration = 8)
-    slice_video(filename, fourcc, frame_cap, time_skip = 148, duration = 13)
-"""
+    clip_video(filename, time_skip = 3, duration = 10)
