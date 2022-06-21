28df766969297e6774cbfb356d2d2f6ae44053d9
jere357 <jeronim96@gmail.com>
2022-06-21 15:41:55 +0200

    Update autokorelacija.py

--- faksfolder/autokorelacija.py
+++ faksfolder/autokorelacija.py
@@ -3,6 +3,22 @@ import torchvision.transforms.functional as TF
 from torchvision.transforms.functional import rgb_to_grayscale
 import matplotlib.pyplot as plt
 from math import floor
+import torch
+
+def pad_stripes(stripes, padding_color = 'black'):
+    padded_stripes = []
+    matrix_dimensions = stripes[0].shape
+    for stripe in stripes:
+        print(stripe.shape)
+        if(padding_color == 'black'):
+            padding_top = torch.zeros((int(matrix_dimensions[0]), floor(matrix_dimensions[1]/2), floor(matrix_dimensions[2])))
+            padding_bot = torch.zeros((int(matrix_dimensions[0]), floor(matrix_dimensions[1]/2), floor(matrix_dimensions[2])))
+        elif(padding_color == 'white'):
+            padding_top = torch.ones((int(matrix_dimensions[0]), floor(matrix_dimensions[1]/2), floor(matrix_dimensions[2])))
+            padding_bot = torch.ones((int(matrix_dimensions[0]), floor(matrix_dimensions[1]/2), floor(matrix_dimensions[2])))
+        padded_stripes.append(torch.cat((padding_top, stripe, padding_bot), dim=1))
+        #print(padded_stripe.shape)
+    return padded_stripes
 
 
 def display_stripe(img):
@@ -18,34 +34,24 @@ def display_img(img):
 
 
 def extract_stripes(image, num_stripes, stripe_width=10):
-    #definiraj listu trakica rangeva [100, 200, 300] and then the slicing looks like:[:,:,100, 100+stripe_width] and so ons
     stripes = []
     range_step = floor(image.shape[2] / num_stripes)
-    print(range_step)
     for i in range(1, image.shape[1], range_step):
-        #last stripe might be out of bounds so just do this
         if i+stripe_width > image.shape[2]:
-            stripes.append(image[:, :, -stripe_width: -1])
+            stripes.append(image[:, :, -stripe_width-1: -1])
             break
         else:
             stripes.append(image[:, :, i : i + stripe_width])
-        display_stripe(stripes[-1])
+        #display_stripe(stripes[-1])
     return stripes
+
 
 
 if __name__ == '__main__':
     image = Image.open('images/antonio1.png')
     x = TF.to_tensor(image)
     x.unsqueeze(0)
-    x_gs = rgb_to_grayscale(x)
-    trakica = x[:, :, 400:405]
-    trakica_gs = x_gs[:, :, 400:405]
-    """
-    display_img(x)
-    display_img(x_gs)
-    display_stripe(trakica)
-    display_stripe(trakica_gs)
-    """
-    print(x.shape)
-    # print(trakica.shape)
     stripes = extract_stripes(x, 9, )
+    padded_stripes = pad_stripes(stripes)
+    display_stripe(stripes[0])
+    display_stripe(padded_stripes[0])
